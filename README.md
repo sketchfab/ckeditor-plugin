a6e390d969f3eefbc86d1f302e783c260e018c7c
Pierre-Antoine Passet <pa.passet@gmail.com>
2013-03-15 18:01:03 +0100

    updated › README.md

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
-ckeditor-plugin
-===============
+Sketchfab CKEditor Plugin
+=========================
 
-CKeditor Sketchfab plugin. Add Sketchfab bbCode via a dialog in the text editor.
+The Sketchfab CKEditor plugin adds a Sketchfab button in a CKEeditor text-editing toolbar.
+This button allows to embed Sketchfab models right in your text.
 
-How to use the plugin
----------------------
+Overview
+--------
 
-- Create a sketchfab/ folder in the plugins/ directory of the CKeditor path
-- Add the plugin files inside this folder
-- Optionally: Other config (button's position, enabling/disabling the plugin ...) is done via the config.js CKEditor
+- Add a sketchfab button in your toolbar
+![Add a sketchfab button in your toolbar](/example/blenderartist/button.png)
+
+- Paste a sketchfab url
+![Sketchfab CKE Plugin dialog](/example/blenderartist/button.png)
+
+- Embed a model in one click
+![Sketchfab embed in a post](/example/blenderartist/button.png)
+
+
+Supported editors
+-----------------
+The CKE plugin can be used on any backend using CKEditor as a frontend text-editor, and able to parse [sketchfab] bbCode
+See examples for implementation details (currently: vBulletin)
+
+Getting started
+---------------
+
+Implementation may vary depending on your backend system (vBulletin, phpBB, wordpress ...).
+Specific implementation details are provided when available, but the main steps are basically the same:
+
+- Create a sketchfab/ folder in the plugins/ directory of the CKeditor
+- Copy the content of the src/ directory inside this folder
+- Edit 'siteName' in plugin.js
+- Edit CKEditor's config (often found in config.js) to enable the plugin and efine the position of the Sketchfab button in your toolbar
+
+
+[sketchfab] bbCode
+------------------
+Your text-editor / backend must support the [sketchfab] bbCode.
+The [sketchfab] bbCode converts a string such as [sketchfab]modelId[/sketchfab] into a Sketchfab embed (iframe).
+Implementation details may vary depending on your backend
--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,11 @@
 CKEDITOR.plugins.add('sketchfab', {
     init: function(editor) {
+        var siteName = 'blenderartists';
         var bbCodeTag = 'sketchfab';
+
+        var trim = function(str) {
+            return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        };
 
         editor.addCommand('openSketchfabDialog', new CKEDITOR.dialogCommand('openSketchfabDialog'));
 
@@ -23,13 +28,14 @@ CKEDITOR.plugins.add('sketchfab', {
                     elements:  [{
                         type: 'html',
                         html: '<img style="float: left; width: 34px; margin-right: 5px; margin-bottom: 8px;" src="' + logoPath + '"/>' +
-                              '<p style="margin-top: 12px; font-weight: bold;">Embed a 3D model from <a style="margin-top: 12px; font-weight: bold;" target="_blank" href="https://sketchfab.com">Sketchfab.com</a> in your post</p>'
+                              '<p style="margin-top: 12px; font-weight: bold;">Embed a 3D model from <a style="margin-top: 12px; font-weight: bold;" target="_blank" href="https://sketchfab.com/?utm_source=' + siteName + '&utm_medium=cke-plugin&utm_content=toplink&utm_campaign=' + siteName +'-cke-plugin"">sketchfab.com</a> in your post</p>'
                     }, {
                         type : 'text',
                         id : 'url',
-                        label : 'Model url',
+                        label : 'Model url <span style="font-style:italic; font-size: 0.8em; line-height: 20px;">(example: https://sketchfab.com/show/x4ATBGtYWDF0yOyoi13xTwG9gkm)</span>',
                         validate : function() {
                             var url = this.getValue();
+                            url = trim(url);
                             var regexp = /^(http|https):\/\/sketchfab.com\/show\/(.+)$/;
                             if (!regexp.test(url)) {
                                 alert('Please enter a valid model url.\n\n' +
@@ -41,12 +47,13 @@ CKEDITOR.plugins.add('sketchfab', {
                         required : true,
                         commit: function(data) {
                             var url = this.getValue();
+                            url = trim(url);
                             var modelId = url.substring(url.lastIndexOf('/') + 1);
                             data.modelId = modelId;
                         }
                     }, {
                         type: 'html',
-                        html: '<p style="font-style:italic; font-size: 0.8em;">Example: https://sketchfab.com/show/x4ATBGtYWDF0yOyoi13xTwG9gkm</p>'
+                        html: '<p style="margin: 10px 0;"><a style="text-decoration:underline;" href="https://sketchfab.com/?utm_source=' + siteName + '&utm_medium=cke-plugin&utm_content=bottomlink&utm_campaign=' + siteName +'-cke-plugin" target="_blank">Sketchfab</a> is a free web service to publish interactive 3D models online without plugin.</p>'
                     }]
                 }],
                 onOk: function() {
@@ -54,7 +61,7 @@ CKEDITOR.plugins.add('sketchfab', {
                         data = {};
                     this.commitContent(data);
 
-                    editor.insertText('[' + bbCodeTag +']' + data.modelId + '[/' + bbCodeTag +']\n');
+                    editor.insertText('[' + bbCodeTag +']' + data.modelId + '[/' + bbCodeTag +']');
                 }
             };
         });
